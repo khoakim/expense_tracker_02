d75127de8fb33ce59710daac645c37ee2fa2c05f
khoakim <khoakim@atosp.onmicrosoft.com>
2021-11-08 08:21:07 +1100

    Check ing

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from "expo-status-bar";
+import React from "react";
+import {StyleSheet, Text, View, Button} from "react-native";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import {NavigationContainer} from "@react-navigation/native";
+
+import CreateScreen from "./src/screens/CreateScreen";
+import EditScreen from "./src/screens/EditScreen";
+import ShowScreen from "./src/screens/ShowScreen";
+import IndexScreen from "./src/screens/IndexScreen";
+import ContextProvider from "./src/context/BlogContext";
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ContextProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Index">
+          <Stack.Screen
+            name="Create"
+            component={CreateScreen}
+            options={{
+              title: "Overview",
+            }}
+          />
+          <Stack.Screen name="Edit" component={EditScreen} />
+          <Stack.Screen name="Show" component={ShowScreen} />
+          <Stack.Screen
+            name="Index"
+            component={IndexScreen}
+            options={({navigation}) => ({
+              headerTitle: (props) => <Text>Index</Text>,
+              headerRight: () => (
+                <Button
+                  onPress={() => navigation.navigate("Create")}
+                  title="Info"
+                  color="#00cc00"
+                />
+              ),
+            })}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </ContextProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
